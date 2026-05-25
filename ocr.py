b1e7844e363cda97e3688f22cb0f677399f61b7f
Tejas M <tejasmcodes@gmail.com>
2026-05-25 14:21:05 +0530

    refactor: converted the experimental scripts to modular functions

--- ocr.py
+++ ocr.py
@@ -1,43 +1,10 @@
 # Handles image to text extraction using Tesseract
-import json
-import re
-from PIL import Image
 import pytesseract
-from pdf2image import convert_from_path
 
-pdf_path = "samples/teelabs.pdf"
-pages = convert_from_path(pdf_path, dpi=300)
-
-for index,page in enumerate(pages):
-    image_name = f"page_{index+1}.png"
-    page.save(f"samples/PDF_images/{image_name}","PNG")
-    print(f"Saved: {image_name}")
+def extract_text(processed_img):
+    text = pytesseract.image_to_string(processed_img)
+    return text
 
 
 
-img = Image.open("samples/PDF_images/page_1.png")
-text = pytesseract.image_to_string(img)
-
-# basic text cleaning
-text  = text.lower()
-text = re.sub(r'[^a-zA-Z0-9\s:/.-]', '', text)
-
-#field extraction
-# invoice date
-date_pattern = r"\d{2}/\d{2}/\d{4}"
-
-# gst num
-gst_pattern = r"\d{2}[A-Z]{5}\d{4}[A-Z]{1}[A-Z\d]{1}[Z]{1}[A-Z\d]{1}"
-
-# invoice number
-invoice_keywords = [
-    "invoice_number",
-    "invoice_no",
-    "invoice#",
-    "bill no"
-]
-print(text)
-
-# to mitigate the problem:
-# perform ->binarization(thresholding), de-skewing, noise remvoval using opencv or pillow
 
--- preprocess.py
+++ preprocess.py
@@ -1,38 +1,53 @@
 import cv2
 from deskew import determine_skew
 
-img = cv2.imread("samples/s6.png")
-original = img.copy()
+def preprocess_image(img_path):
 
-# None -> No Hard fixed sizes, scales the image by a factor of 2(x,y)
-# interpolation is applied to get smoothen and sharp the edge of the enlarges text
-# because small text causes OCR engine to fail
-scaled = cv2.resize(img, None, fx=2.0, fy=2.0 , interpolation = cv2.INTER_CUBIC)
+    img = cv2.imread(img_path)
+    original = img.copy()
 
-# convert to grayscale, because OCR engines struggles with RGB
-grayscale = cv2.cvtColor(scaled, cv2.COLOR_BGR2GRAY)
+    # None -> No Hard fixed sizes, scales the image by a factor of 2(x,y)
+    # interpolation is applied to get smoothen and sharp the edge of the enlarges text
+    # because small text causes OCR engine to fail
+    scaled = cv2.resize(
+        original, 
+        None, 
+        fx=2.0, 
+        fy=2.0 , 
+        interpolation = cv2.INTER_CUBIC
+        )
+    
 
-# deskewing
-angle = determine_skew(grayscale)
-if abs(angle)<10:
+    # convert to grayscale, because OCR engines struggles with RGB
+    grayscale = cv2.cvtColor(
+        scaled, 
+        cv2.COLOR_BGR2GRAY
+        )
 
-    (h, w) = grayscale.shape
-    center = (w//2, h//2)
+    # deskewing
+    angle = determine_skew(grayscale)
+    if abs(angle)<10:
 
-    M = cv2.getRotationMatrix2D(
-                                center, 
-                                angle, 
-                                1.0)
+        (h, w) = grayscale.shape
+        center = (w//2, h//2)
+
+        M = cv2.getRotationMatrix2D(
+                                    center, 
+                                    angle, 
+                                    1.0
+                                    )
 
 
-    processed = cv2.warpAffine(
-                            grayscale, 
-                            M, 
-                            (w, h), 
-                            flags=cv2.INTER_CUBIC, 
-                            borderMode=cv2.BORDER_REPLICATE)
+        processed_img = cv2.warpAffine(
+                                grayscale, 
+                                M, 
+                                (w, h), 
+                                flags=cv2.INTER_CUBIC, 
+                                borderMode=cv2.BORDER_REPLICATE
+                                )
 
-else:
-    processed=  grayscale
+    else:
+        processed_img=  grayscale
 
-cv2.imwrite(f"samples/OCR_ready/s6.png", processed)
+    return processed_img
+
